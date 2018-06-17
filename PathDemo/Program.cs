deb359275c8df8f17a16535dd2a356f2ef52d942
umitliguler <info.umitguler@gmail.com>
2018-06-17 12:03:36 +0300

    first

---
+++ PathDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var fileName = @"c:\vidobu\cs_file.txt";
+
+            Console.WriteLine(Path.GetDirectoryName(fileName));
+            Console.WriteLine(Path.GetFileNameWithoutExtension(fileName));
+            Console.WriteLine(Path.AltDirectorySeparatorChar);
+            Console.WriteLine(Path.VolumeSeparatorChar);
+            Console.WriteLine(Path.GetTempFileName());
+            Console.WriteLine(Path.GetTempPath());
+
+            Console.ReadKey();
+
+        }
+    }
+}
